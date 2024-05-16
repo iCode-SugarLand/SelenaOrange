84caeee793fb858dac4227d059673bad6183b1bb
iCode-SugarLand <124052535+iCode-SugarLand@users.noreply.github.com>
2024-05-15 19:25:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const uwu = new Audio("uwu-sound-119010.mp3");
+
+window.addEventListener("mousemove",()=>{
+  uwu.play();
+});
+const roblox = new Audio("gravity-coil-sound-195556 (1).mp3");
+
+window.addEventListener("click",()=>{
+  roblox.play();
+});
